3fd9cf56223db4208df690fc5e99da8e8371c23d
sepuckett86 <sepuckett86@gmail.com>
2019-05-28 16:15:48 -0700

    add CharacterList and style it, and add characters and character data in test

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import CharacterList from './CharacterList.js';
 
 class App extends Component {
     render() {
         const dom = this.renderDOM();
+
+        const main = dom.querySelector('main');
         
         const header = new Header();
         const headerDOM = header.render();
 
+        const characterList = new CharacterList();
+        const characterListDOM = characterList.render();
+        
         dom.prepend(headerDOM);
+        main.appendChild(characterListDOM);
 
         return dom;
     }
---
+++ src/components/CharacterList.js
@@ -0,0 +1,24 @@
+import Component from './Component.js';
+
+class CharacterList extends Component {
+    renderTemplate() {
+        return /*html*/ `
+            <ul>
+                <li>
+                    <h2>46th Earth King</h2>
+                    <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441">
+                </li>
+                <li>
+                    <h2>Aang</h2>
+                    <img src="https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003">
+                </li>
+                <li>
+                    <h2>Aang (games)</h2>
+                    <img src="https://vignette.wikia.nocookie.net/avatar/images/2/21/Into_The_Inferno_Aang.png/revision/latest?cb=20131009060746">
+                </li>
+            </ul>
+        `;
+    }
+}
+
+export default CharacterList;
