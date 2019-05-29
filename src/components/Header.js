dbeb2b61478d447311f304028f83f62f14d288c1
sepuckett86 <sepuckett86@gmail.com>
2019-05-29 15:04:35 -0700

    enable nation buttons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import Component from './Component.js';
 
+import stringToParams from '../string-to-params.js';
+
 class Header extends Component {
     renderTemplate() {
+        const waterParams = stringToParams('nation', 'water');
+        const earthParams = stringToParams('nation', 'earth');
+        const fireParams = stringToParams('nation', 'fire');
+        const airParams = stringToParams('nation', 'air');
+
         return /*html*/ `
             <header>
                 <section>
-                    <img src="./assets/water.png">
-                    <img src="./assets/earth.png">
+                    <a href="#${waterParams}"><img src="./assets/water.png"></a>
+                    <a href="#${earthParams}"><img src="./assets/earth.png"></a>
                     <h1>Avatar Characters</h1>
-                    <img src="./assets/fire.png">
-                    <img src="./assets/air.png">
+                    <a href="#${fireParams}"><img src="./assets/fire.png"></a>
+                    <a href="#${airParams}"><img src="./assets/air.png"></a>
                 </section>
             </header>
         `;
